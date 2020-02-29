680382a4908cf6dfff4164a711ae63ba27799f74
nelsonic <nelson+github@dwyl.io>
2020-02-29 22:16:05 +0000

    update keys in parse to use underscores like in Phoenix app

--- test/parse.test.js
+++ test/parse.test.js
@@ -8,9 +8,8 @@ test('parse the bounce sns notification event', function (t) {
   // console.log(json);
   // console.log(' - - - - - - - - ');
   const mid = '0102017092006798-f0456694-ac24-487b-9467-b79b8ce798f2-000000';
-  t.equal(json.messageId, mid, "messageId is: " + json.messageId);
-  t.equal(json.notificationType, "Bounce Permanent",
-    "Type: " + json.notificationType)
+  t.equal(json.message_id, mid, "message_id is: " + json.message_id);
+  t.equal(json.status, "Bounce Permanent", "Type: " + json.status)
   t.end();
 });
 
@@ -18,7 +17,6 @@ test('attempt to parse an SNS event without event.Records', function (t) {
   const event = require('./fixtures/sample_sns_bounce.json');
   delete event.Records;
   const json = parse(event)
-
   t.deepEqual(json, {}, "Does not choke when SNS event is invalid");
   t.end();
 });
