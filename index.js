0121f1a4904d7aed026eb53b43d2b9789444e7e9
nelsonic <nelson+github@dwyl.io>
2020-02-29 22:16:22 +0000

    invoke parse in index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("env2")(".env");
 const debug = require("./lib/debug.js");
 const send = require("./lib/send.js");
+const parse = require('./lib/parse.js');
 
 exports.handler = function handler (event, context, callback) {
   debug(event);
@@ -9,6 +10,8 @@ exports.handler = function handler (event, context, callback) {
   	return send(event, callback);
   }
   else {
+    const json = parse(event);
+    
     return callback(null, event)
   }
 }
