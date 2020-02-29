e29476cdd691e85fb61e68de36eb49074548a14f
nelsonic <nelson+github@dwyl.io>
2020-02-29 17:29:25 +0000

    debug context

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const debug = require("./lib/debug.js");
 const send = require("./lib/send.js");
 
 exports.handler = function handler (event, context, callback) {
+  console.log(process.env);
   debug(event);
+  context.key = "context";
+  debug(context);
+
   if (event.email) { // event contains an email (address), so send!
   	return send(event, callback);
   }
