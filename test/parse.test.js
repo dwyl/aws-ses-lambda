3998a07190925f7daa209f1fda8d3637671b3c18
nelsonic <nelson+github@dwyl.io>
2020-03-01 23:17:52 +0000

    add test for jwt.verify to confirm data is retrievable

--- test/parse.test.js
+++ test/parse.test.js
@@ -5,8 +5,8 @@ test('parse the bounce sns notification event', function (t) {
   const event = require('./fixtures/sample_sns_bounce.json');
 
   const json = parse(event)
-  // console.log(json);
-  // console.log(' - - - - - - - - ');
+  console.log(json);
+  console.log(' - - - - - - - - ');
   const mid = '0102017092006798-f0456694-ac24-487b-9467-b79b8ce798f2-000000';
   t.equal(json.message_id, mid, "message_id is: " + json.message_id);
   t.equal(json.status, "Bounce Permanent", "Type: " + json.status)
